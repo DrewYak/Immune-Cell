0919efe254d978ea812f7d95bb24298825c3ead1
DrewYak <DrewYak7@gmail.com>
2022-10-23 21:29:44 +0300

    init

---
+++ src/states/StartState.lua
@@ -0,0 +1,60 @@
+--[[
+    -- StartState Class --
+
+    Author: Iakovlichev Andrew
+    DrewYak7@gmail.com
+
+    Represents the state the game is in when we've just started; should
+    simply display "Immune Cell" in large text, as well as a message to press
+    Enter to begin.
+]]
+
+-- the "__includes" bit here means we're going to inherit all of the methods
+-- that BaseState has, so it will have empty versions of all StateMachine methods
+-- even if we don't override them ourselves; handy to avoid superfluous code!
+StartState = Class{__includes = BaseState}
+
+-- whether we're highlighting "Start" or "High Scores"
+local highlighted = 1
+
+function StartState:update(dt)
+    -- toggle highlighted option if we press an arrow key up or down
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then
+        highlighted = 1 + highlighted % 2
+    end
+
+    -- we no longer have this globally, so include here
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+function StartState:render()
+    -- title
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf("IMMUNE CELL", 0, VIRTUAL_HEIGHT / 3,
+        VIRTUAL_WIDTH, 'center')
+    
+    -- instructions
+    love.graphics.setFont(gFonts['medium'])
+
+    -- if we're highlighting 1, render that option blue
+    if highlighted == 1 then
+        love.graphics.setColor(103/255, 1, 1, 1)
+    end
+    love.graphics.printf("START", 0, VIRTUAL_HEIGHT / 2 + 70,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(1, 1, 1, 1)
+
+    -- render option 2 blue if we're highlighting that one
+    if highlighted == 2 then
+        love.graphics.setColor(103/255, 1, 1, 1)
+    end
+    love.graphics.printf("HIGH SCORES", 0, VIRTUAL_HEIGHT / 2 + 140,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(1, 1, 1, 1)
+end
