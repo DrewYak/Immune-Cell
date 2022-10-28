c06ac66b9ebcee9e77c0ef911570325dbf326b2d
DrewYak <DrewYak7@gmail.com>
2022-10-28 22:46:00 +0300

    Вирус вращается и движется по вертикали, отражаясь от стенок

--- src/Virus.lua
+++ src/Virus.lua
@@ -4,12 +4,13 @@ function Virus:init(number)
 	self.x = 0
 	self.y = VIRTUAL_HEIGHT / 2
 	self.r = 0
+	self.direction = math.random(0, 1) * 2 - 1
 
-	self.dx = VIRUS_SPEED
-	self.dy = 0
+	self.dx = VIRUS_SPEED_X
+	self.dy = math.random(VIRUS_SPEED_Y / 10, VIRUS_SPEED_Y)
 	self.dr = VIRUS_ROT_SPEED
 
-	self.width =256
+	self.width = 256
 	self.height = 256
 
 	self.number = number
@@ -19,13 +20,16 @@ end
 
 function Virus:update(dt)
 	self.x = self.x + self.dx * dt 
-	self.y = self.y + self.dy * dt 
-	self.r = self.r + self.dr
+	if (self.y <= self.height * VIRUS_SCALE / 2) or (self.y >= VIRTUAL_HEIGHT - self.height * VIRUS_SCALE / 2) then
+		self.direction = -self.direction
+	end
+	self.y = self.y + self.direction * self.dy * dt
+	self.r = self.r + self.dr * dt
 end
 
 function Virus:render()
 	love.graphics.draw(gTextures['viruses'],
 		gFrames['viruses'][self.number],
 		self.x, self.y, self.r, 
-		1, 1, self.width / 2, self.height / 2)
+		VIRUS_SCALE, VIRUS_SCALE, self.width / 2, self.height / 2)
 end
